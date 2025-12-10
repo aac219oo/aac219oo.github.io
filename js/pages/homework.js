64c1de17d65fcafee6a2178ecb883bdf8b789f40
James Hsu <james.hsu@twjoin.com>
2025-12-10 19:52:25 +0800

    上傳作業100220678、100220208

--- js/pages/homework.js
+++ js/pages/homework.js
@@ -13,7 +13,9 @@ const Homework = {
                 <li><router-link to="/HW/100218476" class="hover:text-primary hover:underline hover:decoration-solid">05.表格練習</router-link></li>
                 <li><router-link to="/HW/100219151" class="hover:text-primary hover:underline hover:decoration-solid">07.課堂練習2</router-link></li>
                 <li><router-link to="/HW/100219359" class="hover:text-primary hover:underline hover:decoration-solid">08.課堂作業</router-link></li>
+                <li><router-link to="/HW/100220208" class="hover:text-primary hover:underline hover:decoration-solid">12.課堂練習</router-link></li>
                 <li><router-link to="/HW/100220477" class="hover:text-primary hover:underline hover:decoration-solid">13.全景及3D網頁</router-link></li>
+                <li><a href="https://aac219oo.github.io/next_practice/coupon" target="_blank" class="hover:text-primary hover:underline hover:decoration-solid">14.課堂練習</a></li>
             </ul>
     `,
 };
