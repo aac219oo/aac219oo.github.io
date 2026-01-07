9bb2c4161eee7d79e0cc27aa4d91b0c3120efb17
James Hsu <james.hsu@twjoin.com>
2026-01-07 19:16:24 +0800

    feat: add final exam page with English and Traditional Chinese i18n support

--- js/pages/HW/100221395.js
+++ js/pages/HW/100221395.js
@@ -1,8 +1,8 @@
 const HW100221395 = {
     template: `
         <ol>
-            <li><router-link to="/HW/100221395/section1">題目 1</router-link></li>
-            <li><router-link to="/HW/100221395/section2">題目 2</router-link></li>
+            <li><router-link to="/HW/100221395/section1" target="_blank">題目 1 - RWD 彈性圖文卡片</router-link></li>
+            <li><router-link to="/HW/100221395/section2" target="_blank">題目 2 - 語意化結構與表格</router-link></li>
         </ol>
     `
 };
